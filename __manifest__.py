a045764ad5ba35ab8dba093d7ef4d190dd8e4dc3
rjOwl <mostafayossef100@yahoo.com>
2022-06-21 22:59:24 +0200

    Created report view

--- __manifest__.py
+++ __manifest__.py
@@ -2,12 +2,14 @@
     "name": "hms",
     "depends": ["crm"],
     "data": [
-        "security/hms_security.xml",
-        "security/ir.model.access.csv",
         "views/hms_patient_views.xml",
         "views/hms_department_views.xml",
         "views/hms_doctor_views.xml",
         "views/hms_menus.xml",
-        "views/res_partner_views.xml"
+        "views/res_partner_views.xml",
+        "security/hms_security.xml",
+        "security/ir.model.access.csv",
+        "reports/hms_templates.xml",
+        "reports/hms_reports.xml"
     ]
 }
