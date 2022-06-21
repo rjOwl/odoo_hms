9ea8fe9aba1bd16c66b7d1ecc1734d57e2daab21
rjOwl <mostafayossef100@yahoo.com>
2022-06-21 22:10:42 +0200

    Added res partners

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,11 @@
 {
     "name": "hms",
+    "depends": ["crm"],
     "data": [
         "views/hms_patient_views.xml",
         "views/hms_department_views.xml",
         "views/hms_doctor_views.xml",
-        "views/hms_menus.xml"
+        "views/hms_menus.xml",
+        "views/res_partner_views.xml"
     ]
 }
--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,1 @@
-from . import hms_patient
-from . import hms_department
-from . import hms_doctor
+from . import hms_patient, hms_department, hms_doctor, res_partner
