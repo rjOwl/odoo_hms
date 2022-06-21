72e6e837e5b7b4adec9d590bcb1745504708ae04
rjOwl <mostafayossef100@yahoo.com>
2022-06-21 22:17:34 +0200

    Validating patient email

--- model/res_partner.py
+++ model/res_partner.py
@@ -1,7 +1,13 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     related_patient_id = fields.Many2one("hms.patient")
+
+    @api.constrains("related_patient_id")
+    def _validate_email(self):
+        if self.related_patient_id.email and self.related_patient_id.email != self.email:
+            raise ValidationError("Patient has a Different Email")
